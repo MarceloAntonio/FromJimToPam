2ff8def24abfdda9a6e4aeb6b9c95df2ee9b6876
Marcelo Antonio <marceloantonio.contato12@gmail.com>
2026-06-30 16:57:04 -0300

    Corrigido 2 bugs: 2 fclose | \r\n podendo corromper o while principal | E alguns outputs reescritos

--- main.c
+++ main.c
@@ -21,62 +21,63 @@ int main(int argc, char *argv[]) {
 
     // Functions
     void OpenNotepad() {
-        printf("[INFO] Abrindo arquivo para edicao...\n");
+        printf("[INFO] Abrindo editor de texto...\n");
         sprintf(command, "start %s%s%s", userPath, CONFIG_SUBDIR, CONFIG_FILENAME);
         system(command);
-        printf("[INFO] Finalizando\n");
-        fclose(file);
+        printf("[INFO] Editor encerrado.\n");
         exit(0);
     }
-
+    
+    // Code 
     if (argc > 1 && strcmp(argv[1], "-e") == 0 && file != NULL) {
         OpenNotepad();
     }
 
-    printf("[INFO] Verificando arquivo de configuracao: %s\n", dirfile);
+    printf("[INFO] Buscando arquivo em: %s\n", dirfile);
 
     if (file == NULL) {
-        printf("[INFO] Arquivo nao encontrado. Inicializando configuracao...\n");
+        printf("[INFO] Arquivo nao encontrado. Criando configuracao inicial...\n");
         char response[50];
 
         char dirPath[MAX_PATH_LEN];
         sprintf(dirPath, "%s%s", userPath, CONFIG_SUBDIR);
-        printf("[INFO] Criando diretorio: %s\n", dirPath);
+        printf("[INFO] Criando pasta: %s\n", dirPath);
         _mkdir(dirPath);
 
         file = fopen(dirfile, "w");
-        fprintf(file, "# Coloque os links/programas um abaixo do outro");
+        fprintf(file, "# Coloque os links ou programas um abaixo do outro\n");
         fclose(file);
         printf("[INFO] Arquivo criado com sucesso!\n");
-        printf("Edite o arquivo %s em %s\n", CONFIG_FILENAME, CONFIG_SUBDIR);
-        printf("Voce deseja editar o arquivo agora? \n(S/N): ");
+        printf("[INFO] Caminho do arquivo: %s\n", dirfile);
+        printf("Deseja abrir o arquivo para editar agora? [S/N]: ");
         fgets(response, 50, stdin);
         response[0] = tolower(response[0]);
 
         if (response[0] == 's') {
             OpenNotepad();
         } else {
-            printf("[INFO] Finalizando...\n");
+            printf("[INFO] Programa finalizado.\n");
             return 0;
         }
     }
 
-    printf("[INFO] Arquivo encontrado! Lendo alvos para inicializacao...\n");
+    printf("[INFO] Arquivo encontrado. Lendo lista de alvos...\n");
 
     while (fgets(line, sizeof(line), file) != NULL) {
+        line[strcspn(line, "\r\n")] = 0;
         if (strlen(line) == 0) continue;
 
         if (line[0] == '#') {
             continue;
         }
 
-        printf("[INFO] Iniciando alvo: %s", line);
+        printf("[INFO] Abrindo item: %s\n", line);
         sprintf(command, "start \"\" \"%s\"", line);
 
         system(command);
     }
 
-    printf("\n[INFO] Execucao concluida com sucesso!\n");
+    printf("\n[INFO] Todos os alvos foram abertos.\n");
     fclose(file);
     return 0;
 } 
