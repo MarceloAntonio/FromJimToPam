fd0009a4b2151f97f98b9d9b2cb74803196a39a4
Marcelo Antonio <marceloantonio.contato12@gmail.com>
2026-07-01 09:57:33 -0300

    Código refatorado, separando o main.c em funções

--- main.c
+++ main.c
@@ -8,59 +8,74 @@
 #define CONFIG_FILENAME "targets.conf"
 #define MAX_PATH_LEN 512
 
-int main(int argc, char *argv[]) {
+    void MontarPathFile(char *buffer){
+        char *userPath = getenv("USERPROFILE");
+        if (userPath != NULL){
+            snprintf(buffer, MAX_PATH_LEN, "%s%s%s", userPath, CONFIG_SUBDIR, CONFIG_FILENAME);
+        }
+    }
+    
+    void MontarPathAppData(char *buffer){
+        char *userPath = getenv("USERPROFILE");
+        if (userPath != NULL){
+            snprintf(buffer, MAX_PATH_LEN, "%s%s", userPath, CONFIG_SUBDIR);
+        }
+    }
 
-    // Variables
-    char *userPath = getenv("USERPROFILE");
-    FILE *file;
-    char command[MAX_PATH_LEN];
-    char line[MAX_PATH_LEN];
-    char dirfile[MAX_PATH_LEN];
-    sprintf(dirfile, "%s%s%s", userPath, CONFIG_SUBDIR, CONFIG_FILENAME);
-    file = fopen(dirfile, "r");
-
-    // Functions
-    void OpenNotepad() {
+    void OpenNotepad(const char *pathFile) {
+        char command[MAX_PATH_LEN];
         printf("[INFO] Abrindo editor de texto...\n");
-        sprintf(command, "start %s%s%s", userPath, CONFIG_SUBDIR, CONFIG_FILENAME);
+        snprintf(command,sizeof(command), "start notepad %s", pathFile);
         system(command);
         printf("[INFO] Editor encerrado.\n");
         exit(0);
     }
-    
-    // Code 
-    if (argc > 1 && strcmp(argv[1], "-e") == 0 && file != NULL) {
-        OpenNotepad();
-    }
 
-    printf("[INFO] Buscando arquivo em: %s\n", dirfile);
-
-    if (file == NULL) {
+    void FileDontFinded( const char *PathFile){
         printf("[INFO] Arquivo nao encontrado. Criando configuracao inicial...\n");
         char response[50];
+        char PathAppData[MAX_PATH_LEN];
+        strcpy(PathAppData, PathFile);
 
-        char dirPath[MAX_PATH_LEN];
-        sprintf(dirPath, "%s%s", userPath, CONFIG_SUBDIR);
-        printf("[INFO] Criando pasta: %s\n", dirPath);
-        _mkdir(dirPath);
+        char *ultima_barra = strrchr(PathAppData, '\\');
 
-        file = fopen(dirfile, "w");
+        if (ultima_barra != NULL) {
+            *(ultima_barra + 1) = '\0'; 
+        }
+        
+        printf("[INFO] Criando pasta: %s\n", PathAppData);
+        _mkdir(PathAppData);
+        
+        FILE *file = fopen(PathFile, "w");
         fprintf(file, "# Coloque os links ou programas um abaixo do outro\n");
         fclose(file);
+
         printf("[INFO] Arquivo criado com sucesso!\n");
-        printf("[INFO] Caminho do arquivo: %s\n", dirfile);
+        printf("[INFO] Caminho do arquivo: %s\n", PathFile);
         printf("Deseja abrir o arquivo para editar agora? [S/N]: ");
         fgets(response, 50, stdin);
         response[0] = tolower(response[0]);
 
         if (response[0] == 's') {
-            OpenNotepad();
+            OpenNotepad(PathFile);
         } else {
             printf("[INFO] Programa finalizado.\n");
-            return 0;
+            exit(0);
         }
     }
 
+    void Scanning(const char *PathFile){
+    char line[MAX_PATH_LEN];
+    FILE *file = fopen(PathFile, "r");
+    char command[MAX_PATH_LEN];
+
+
+    if(file == NULL){
+        FileDontFinded(PathFile);
+    }
+
+    printf("[INFO] Buscando arquivo em: %s\n", PathFile);
+    
     printf("[INFO] Arquivo encontrado. Lendo lista de alvos...\n");
 
     while (fgets(line, sizeof(line), file) != NULL) {
@@ -72,12 +87,28 @@ int main(int argc, char *argv[]) {
         }
 
         printf("[INFO] Abrindo item: %s\n", line);
-        sprintf(command, "start \"\" \"%s\"", line);
+        snprintf(command,sizeof(command) ,"start \"\" \"%s\"", line);
 
         system(command);
     }
 
     printf("\n[INFO] Todos os alvos foram abertos.\n");
     fclose(file);
+    }
+    
+int main(int argc, char *argv[]) {
+
+    char pathFile[MAX_PATH_LEN];
+
+    MontarPathFile(pathFile);
+ 
+    if (argc > 1 && strcmp(argv[1], "-e") == 0) {
+        OpenNotepad(pathFile);
+    }
+    
+    Scanning(pathFile);
+    
     return 0;
+
+
 } 
